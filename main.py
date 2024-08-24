0c4bcc0ec6b244b6cf40af80628af06087471549
Davi <davicamiloribeiro@gmail.com>
2024-08-24 13:09:05 -0300

    Consumindo Get countries

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from football.teams import getTeamInformation
 from general.timezone import getTimezones
+from general.countries import getCountries
 
 #getTeamInformation(17)
 
-getTimezones()
+#getTimezones()
+getCountries()
