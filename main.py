4fd793966e0eaef7febbc69d8aa97fb6c5e0589d
Davi <davicamiloribeiro@gmail.com>
2024-08-24 13:47:25 -0300

    Consumindo Get leagues

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from football.teams import getTeamInformation
 from general.timezone import getTimezones
 from general.countries import getCountries
+
+from football.leagues import getLeagues
+from football.teams import getTeamInformation
 
 #getTeamInformation(17)
 
 #getTimezones()
-t = getCountries()
+#getCountries()
+t = getLeagues()
 print(t)
