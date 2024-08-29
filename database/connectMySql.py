54358dc1fb4ffafd971d44c4b07cafcbba480416
Davi <davicamiloribeiro@gmail.com>
2024-08-29 20:48:32 -0300

    Guardando dados de acesso

--- database/connectMySql.py
+++ database/connectMySql.py
@@ -1,14 +1,19 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
 from mysql.connector import Error
+
+
+load_dotenv()
 
 def conectar():
     try:
         # Configurações de conexão
         conn = mysql.connector.connect(
-            host='localhost',
-            user='seu_usuario',
-            password='sua_senha',
-            database='nome_do_banco_de_dados'
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DATABASE')
         )
 
         if conn.is_connected():
--- main.py
+++ main.py
@@ -14,3 +14,28 @@ from football.transfers import getTransfers
 from football.trophies import getTrophies
 from football.sidelined import getSidelined
 
+from database.connectMySql import conectar
+
+conn = conectar()
+
+if conn:
+    cursor = conn.cursor()
+
+    # Exemplo de operação: Criando uma tabela
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS funcionarios (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL,
+            cargo VARCHAR(255),
+            salario DECIMAL(10, 2),
+            data_contratacao DATE
+        )
+    """)
+    
+    # Confirmar a operação
+    conn.commit()
+    
+    # Fechar a conexão
+    cursor.close()
+    conn.close()
+    print("Operação realizada e conexão fechada.")
