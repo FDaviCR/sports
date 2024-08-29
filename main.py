807afb6d85afb5f3f591f79298c5fb60a9e376ea
Davi <davicamiloribeiro@gmail.com>
2024-08-28 21:10:19 -0300

    Consumindo Get Transfers

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ from football.standings import getStandings
 from football.fixtures import getRounds, getFixtures, getHeadToHead, getStatistics, getEvents, getLineups, getPlayers
 from football.injuries import getInjuries
 from football.coachs import getCoachs
-
+from football.players import getPlayers, getSeasons, getSquads, getTopScorers, getTopAssists, getTopYellowCards, getTopRedCards
+from football.venues import getVenues
+from football.predictions import getPredictions
+from football.transfers import getTransfers
